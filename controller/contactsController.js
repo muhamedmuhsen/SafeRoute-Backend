dc38894d3bb67d02d3468bb457df00c196a9a733
Mohamed Mohsen <mohamedm.abdlnabi@gmail.com>
2025-07-10 07:22:37 +0300

    Add functionality to manage trusted contacts: implement addTrustedContact endpoint and update routes to handle contact addition alongside retrieval.

--- controller/contactsController.js
+++ controller/contactsController.js
@@ -6,12 +6,11 @@ import User from "../model/user.model.js";
 
 const getTrustedContacts = asyncWrapper(async (req, res, next) => {
   try {
-
     const userId = req.user._id;
 
     const contacts = await Contact.find({ user: userId });
-    console.log(contacts);
-    
+
+
     res
       .status(200)
       .json({ success: httpStatusText.SUCCESS, data: { contacts } });
@@ -25,6 +24,46 @@ const getTrustedContacts = asyncWrapper(async (req, res, next) => {
   }
 });
 
-export {
-  getTrustedContacts,
-};
+const addTrustedContact = asyncWrapper(async (req, res, next) => {
+  try {
+    const user = req.user;
+    const contact = req.body;
+
+    if (!user) {
+      const err = appError.create(
+        "error while getting the user",
+        400,
+        httpStatusText.FAIL
+      );
+      return next(err);
+    }
+
+    if (!contact.name || !contact.phone) {
+      const err = appError.create(
+        "Contact name and phone are required",
+        400,
+        httpStatusText.FAIL
+      );
+      return next(err);
+    }
+
+    const addedContact = new Contact({
+      name: contact.name,
+      phone: contact.phone,
+      user: user._id,
+    });
+
+    await addedContact.save();
+
+    res.status(201).json({ status: httpStatusText.SUCCESS, data: addedContact });
+  } catch (error) {
+    const err = appError.create(
+      "Error while adding contacts",
+      500,
+      httpStatusText.ERROR
+    );
+    return next(err);
+  }
+});
+
+export { getTrustedContacts, addTrustedContact };
--- routes/contacts.route.js
+++ routes/contacts.route.js
@@ -1,12 +1,14 @@
 import express from "express";
-import authMiddleware from '../middleware/authMiddleware.js'
+import authMiddleware from "../middleware/authMiddleware.js";
 
 import {
   getTrustedContacts,
+  addTrustedContact,
 } from "../controller/contactsController.js";
 
 const router = express.Router();
 
-router.route('/').get(authMiddleware, getTrustedContacts)
+router.route("/").get(authMiddleware, getTrustedContacts);
+router.route("/").post(authMiddleware, addTrustedContact);
 
 export default router;
